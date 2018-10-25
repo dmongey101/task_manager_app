3fb3a19a5899254216fc27c322748dd291f0f737
Donal Mongey <donalmongey@gmail.com>
2018-10-25 11:42:15 +0000

    Initial Commit

---
+++ task_manager.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def show_hi():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+        app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
